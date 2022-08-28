6aedf7eb3076ccfbb1e0d4dea4f2a09c82d15dff
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-08-28 14:35:32 +0800

    init site config

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,16 +2,16 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
-  lang: 'en-US',
+  domain: 'urara-experimental.vercel.app',
+  title: 'Urara@Experimental',
+  subtitle: 'Experimental fork of Urara for testing Indiekit, Micropub, etc.',
+  lang: 'zh',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    name: 'John Doe',
-    avatar: '/assets/maskable@192.png',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    name: '藍+85CD',
+    avatar: 'https://kwaa.dev/assets/maskable@192.webp',
+    status: '🌌',
+    bio: 'Experimental fork of Urara for testing Indiekit, Micropub, etc.'
   },
   themeColor: '#3D4451'
 }
